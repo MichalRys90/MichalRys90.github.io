8a8bd6363a0b9ee7687b52354042347afb988100
Michal <mrys@tgo.pl>
2022-11-01 18:51:06 +0100

    Initial commit 4

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://kodilla-tasks-mir.herokuapp.com/v1/tasks';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/boards';
+  const trelloApiRoot = 'https://kodilla-tasks-mir.herokuapp.com/v1/trello/boards';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
